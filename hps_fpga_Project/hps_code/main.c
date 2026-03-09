ac5a5d8da92aaf0033e7d37d4482ccd7f68e9170
Eric Yates <k.ericyates@gmail.com>
2026-03-09 12:28:48 -0400

    main.c updated for testing of apb and state machine

--- hps_fpga_Project/hps_code/main.c
+++ hps_fpga_Project/hps_code/main.c
@@ -11,7 +11,37 @@
 #define HW_REGS_BASE ( ALT_STM_OFST ) 
 #define HW_REGS_SPAN ( 0x04000000 ) 
 #define HW_REGS_MASK ( HW_REGS_SPAN - 1 ) 
- 
+
+// Defintions of pio_fpga_inst and pio_fpga_status functions
+#define INST_RESET 0x00000000
+#define INST_SIGNAL_LOAD_KEY 0x00000001
+#define INST_LOADING_KEY 0x00000003
+#define INST_LOADING_DATA 0x00000005
+#define INST_START_ENCRYPTION 0x00000009
+
+#define STATUS_RESET 0X00000000
+#define STATUS_LOAD_KEY 0x00000003
+#define STATUS_LOAD_DATA 0x00000005
+#define STATUS_ENCRYPTING 0x00000009
+#define STATUS_DONE 0x0000000F
+
+// Definitions of apb offsets
+#define INSTRUCTION_BASE 0x00000000
+#define STATUS_BASE 0x00000004
+#define AES_KEY_BASE 0x00000008 // 128 bit key, 4 32-bit words
+#define AES_PTEXT_BASE 0x00000018 // Plain Text 128 bit data, 4 32-bit words
+#define AES_CTEXT_BASE 0x00000028 // Cipher Text 128 bit data, 4 32-bit words
+
+// Future helper function to map status and instruction to led
+// pio bits [3:0] are apb STATUS BASE bits [3:0]
+// pio bits [9:6] are apb INSTRUCTION BASE bits [3:0]
+
+// helper function for setting apb pointer
+void *set_apb_pointer(void *virtual_base, unsigned long offset) {
+    return virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + offset) &
+        (unsigned long)(HW_REGS_MASK));
+}
+
 int main() { 
  
     void *virtual_base; 
@@ -20,8 +50,13 @@ int main() {
     int led_direction; 
     int led_mask;
 	void *pio_led;
-	void *h2p_lw_coproc_addr_memory;
+	void *apb_32x16; // eric_ip2_0
      
+    int mem_data, mm_reg, j, ii; 
+    int most_sig_bit, least_sig_bit;
+    int aes_key[4];
+    int aes_data[4];
+
     // map the address space for the LED registers into user space so we can interact with them. 
     // we'll map in the entire CSR span of the HPS since we want to access various registers within that span 
     printf("Calling fopen\n"); 
@@ -44,15 +79,17 @@ int main() {
 	pio_led = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + PIO_LED_BASE) & 
         (unsigned long)(HW_REGS_MASK)); 
 	
-	h2p_lw_coproc_addr_memory = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE) & 
-        (unsigned long)(HW_REGS_MASK));
+	apb_32x16 = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE) & 
+         (unsigned long)(HW_REGS_MASK));
+    
+    // could use but hides the actual pointer math, which is helpful to see in this example
+    //apb_32x16 = set_apb_pointer(virtual_base, 0); 
          
-
     loop_count = 0; 
     led_mask = 0x01; 
     led_direction = 0; // 0: left to right direction 
     printf("Starting while loop, base address: %p, virtual_base = %p\n", pio_led, virtual_base); 
-    while(loop_count < 2) { 
+    while(loop_count < 1) { 
         printf("In Loop, loop_count:%d, led_mask:%d, led_direction:%d\n", loop_count, led_mask, led_direction); 
         // control led 
         *(uint32_t *)pio_led = ~led_mask;  
@@ -74,27 +111,118 @@ int main() {
         }    
     } // while
 
-    int mem_data, mm_reg, ii;
-    
-    for (ii = 0; ii < 32; ii+=4){
-        printf("Enter coprocessor memory data in hexadecimal, to write in location = %x\n", ii);
+    // Turn all LEDs off
+    *(uint32_t *)pio_led = 0xFF;
+    usleep(100*5000); 
+    *(uint32_t *)pio_led = 0x00; // Turn all LEDs on
+    usleep(100*5000);  
+    *(uint32_t *)pio_led = 0xFF; // Turn all LEDs off
+
+    // Test read and write to custom IP memory mapped registers
+    int test_data= 0x10101010;
+    for (ii = 0; ii < 64; ii+=4){
+        printf("Writing test data: %x to memory address = %p\n", test_data, apb_32x16);
         mm_reg = ii;
-        scanf("%x", &mem_data);
-        h2p_lw_coproc_addr_memory = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
+        apb_32x16 = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
             (unsigned long)(HW_REGS_MASK));
-        printf("Writing coprocessor memory address = %p\n", h2p_lw_coproc_addr_memory);
-        *(uint32_t *)h2p_lw_coproc_addr_memory = mem_data;
+        *(uint32_t *)apb_32x16 = test_data;
+        test_data += 0x10101010;
     }
 
     printf("Reading memory locations\n");
-    for (ii = 0; ii < 32; ii+=4){
+    for (ii = 0; ii < 64; ii+=4){
         mm_reg = ii;
-        h2p_lw_coproc_addr_memory = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
+        apb_32x16 = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
             (unsigned long)(HW_REGS_MASK));
-        printf("Reading coprocessor memory address = %p\n", h2p_lw_coproc_addr_memory);
-        mem_data = *(uint32_t *)h2p_lw_coproc_addr_memory;
-        printf("Memory data read: %x\n", mem_data);
+        mem_data = *(uint32_t *)apb_32x16;
+        printf("Reading test data: %x from memory address = %p\n", mem_data, apb_32x16);
     }
+
+    // State machine test for AES coprocessor
+    // Main loop
+    while(1){
+        // set instruction to reset, wait acknowledge
+        apb_32x16 = set_apb_pointer(virtual_base, INSTRUCTION_BASE);
+        *(uint32_t *)apb_32x16 = INST_RESET;
+        apb_32x16 = set_apb_pointer(virtual_base, STATUS_BASE);
+        while(*(uint32_t *)apb_32x16 != STATUS_RESET);
+        printf("Coprocessor is in reset state\n");
+
+        // set instruction to signal load aes key, wait acknowledge
+        apb_32x16 = set_apb_pointer(virtual_base, INSTRUCTION_BASE);
+        *(uint32_t *)apb_32x16 = INST_SIGNAL_LOAD_KEY;
+        apb_32x16 = set_apb_pointer(virtual_base, STATUS_BASE);
+        while(*(uint32_t *)apb_32x16 != STATUS_LOAD_KEY);
+        printf("Coprocessor is ready to load key\n");
+
+        for (ii = 0, most_sig_bit = 31, least_sig_bit = 0; ii < 16; ii += 4, most_sig_bit += 32, least_sig_bit += 32){
+            printf("Enter aes key[%d:%d] in hexadecimal, to write in location = %x\n", most_sig_bit, least_sig_bit, AES_KEY_BASE + ii);
+            scanf("%x", &mem_data);
+            apb_32x16 = set_apb_pointer(virtual_base, AES_KEY_BASE + ii);
+            *(uint32_t *)apb_32x16 = mem_data;
+        }
+
+        // set instruction to load data, wait acknowledge
+        apb_32x16 = set_apb_pointer(virtual_base, INSTRUCTION_BASE);
+        *(uint32_t *)apb_32x16 = INST_LOADING_DATA;
+        apb_32x16 = set_apb_pointer(virtual_base, STATUS_BASE);
+        while(*(uint32_t *)apb_32x16 != STATUS_LOAD_DATA);
+        printf("Coprocessor is ready to load data\n");
+
+        for (ii = 0, most_sig_bit = 31, least_sig_bit = 0; ii < 16; ii += 4, most_sig_bit += 32, least_sig_bit += 32){
+            printf("Enter aes data[%d:%d] in hexadecimal, to write in location = %x\n", most_sig_bit, least_sig_bit, AES_PTEXT_BASE + ii);
+            scanf("%x", &mem_data);
+            apb_32x16 = set_apb_pointer(virtual_base, AES_PTEXT_BASE + ii);
+            *(uint32_t *)apb_32x16 = mem_data;
+        }
+
+        // set instruction to start encryption, wait acknowledge
+        apb_32x16 = set_apb_pointer(virtual_base, INSTRUCTION_BASE);
+        *(uint32_t *)apb_32x16 = INST_START_ENCRYPTION;
+        apb_32x16 = set_apb_pointer(virtual_base, STATUS_BASE);
+        while(*(uint32_t *)apb_32x16 != STATUS_ENCRYPTING);
+        printf("Coprocessor has started encryption\n");
+        while(*(uint32_t *)apb_32x16 != STATUS_DONE);
+        printf("Coprocessor has completed encryption\n");
+
+        printf("Reading encrypted data locations\n");
+        for (j = 0, ii = 0; ii < 16; ii += 4, j++){
+            apb_32x16 = set_apb_pointer(virtual_base, AES_CTEXT_BASE + ii);
+            mem_data = *(uint32_t *)apb_32x16;
+            aes_data[j] = mem_data;
+            printf("Memory data read [%x]: %x\n", AES_CTEXT_BASE + ii, mem_data);
+        }
+
+        // Print 128-bit encrypted value MSW first
+        printf("Encrypted data is:\n");
+        for (j = 3; j >= 0; j--){
+            printf("%08x ", aes_data[j]);
+        }
+        printf("\n\n");
+    }
+
+
+
+    //====== MANUAL TEST ======================================================
+    // for (ii = 0; ii < 32; ii+=4){
+    //     printf("Enter coprocessor memory data in hexadecimal, to write in location = %x\n", ii);
+    //     mm_reg = ii;
+    //     scanf("%x", &mem_data);
+    //     apb_32x16 = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
+    //         (unsigned long)(HW_REGS_MASK));
+    //     printf("Writing coprocessor memory address = %p\n", apb_32x16);
+    //     *(uint32_t *)apb_32x16 = mem_data;
+    // }
+
+    // printf("Reading memory locations\n");
+    // for (ii = 0; ii < 32; ii+=4){
+    //     mm_reg = ii;
+    //     apb_32x16 = virtual_base + ((unsigned long)(ALT_LWFPGASLVS_OFST + ERIC_IP2_0_BASE + mm_reg) &
+    //         (unsigned long)(HW_REGS_MASK));
+    //     printf("Reading coprocessor memory address = %p\n", apb_32x16);
+    //     mem_data = *(uint32_t *)apb_32x16;
+    //     printf("Memory data read: %x\n", mem_data);
+    // }
 
     // clean up our memory mapping and exit 
         if( munmap(virtual_base, HW_REGS_SPAN) != 0) { 
